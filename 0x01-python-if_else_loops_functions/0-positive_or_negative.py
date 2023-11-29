867bcc6d1ff848c829384989f39d78231391781d
Danmuumbi <muuomuumbi@gmail.com>
2023-11-29 11:22:33 -0800

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+
+# Assigning a random signed number to the variable 'number'
+number = random.randint(-10, 10)
+
+# Printing the assigned number
+print(number, end=' ')
+
+# Checking if the number is positive, negative, or zero
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
