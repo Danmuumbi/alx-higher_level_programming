72cb5029c224cf1cfd7f8e313cf5878601898b1a
Danmuumbi <muuomuumbi@gmail.com>
2024-01-08 21:10:31 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
 Module for lookup function
+
+Prototype: def lookup(obj)
+Returns a list object
 """
-
-
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
