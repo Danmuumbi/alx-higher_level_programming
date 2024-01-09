8a62f747bd55d5542d1fd6920c6d5e86c52d4846
Danmuumbi <muuomuumbi@gmail.com>
2024-01-09 09:40:18 -0800

    9-rectangle.py

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
+
+
 class BaseGeometry:
     """Reprsent base geometry."""
-
     def area(self):
         """Not yet implemented."""
         raise Exception("area() is not implemented")
